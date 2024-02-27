69d0f91c4cceeca5de5b628c32ad4f01b392b9d9
nickhoggett <nick.hoggett@gmail.com>
2024-02-27 20:44:21 +0000

    commit from 3:10

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-const _ = require('lodash')
+const {readFile, writeFile} = require('fs').promises
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flatMapDeep(items)
-console.log(newItems)
+const start = async() => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf8')
+        const second = await readFile('./content/second.txt', 'utf8')
+        await writeFile('./content/result-mind-genade.txt', `THIS IS AWESOME: ${first} ${second}`, 
+            { flag: 'a' }  
+        )
+    } catch (error) {
+        console.log(error)
+    }
+}
 
+start()
+
+// const getText = (path) => {
+//     return new Promise ((resolve, reject) => {
+//         readFile (path, 'utf8', (err, data) => {
+//             if (err) {
+//                 reject(err)
+//             } else {
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
+
+// getText('./content/first.txt')
+//     .then((result) => console.log(result))
+//     .catch((err) => console.log(err))
