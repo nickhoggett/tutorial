e3925372e0829c2885ad26d328bcc5536c754124
nickhoggett <nick.hoggett@gmail.com>
2024-03-04 21:42:23 +0000

    commit 7:22

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,26 +1,36 @@
 const express = require('express')
 const app = express()
+let { people } = require('./data')
 
-const logger = require('./logger')
-const authorise = require('./authorise')
+//static assets
+app.use(express.static('./methods-public'))
 
-// req => middleware => res
+//parse form data 
+app.use(express.urlencoded({ extended: false}))
 
-app.use([logger, authorise])
+//parse data
+app.use(express.json())
 
-app.get('/',(req, res) => {
-  res.send('Home')
+app.get('/api/people', (req, res ) => {
+  res.status(200).json({success: true, data: people})
 })
-app.get('/about', (req, res) => {
-  res.send('About')
+
+app.post('/api/people', (req, res) => {
+  const name = req.body
+  if(!name) {
+    return res.status(400).json({success: false , msg: 'please provide a name on the form'})
+  }
+  res.status(201).send({success: true, person: name})
 })
-app.get('/api/products', (req, res) => {
-  res.send('Products')
+
+app.post('/login', (req, res) => {
+  const {name} = req.body
+  if(name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
+  res.status(401).send('Please provide credentials')
 })
-app.get('/api/items', (req, res) => {
-  console.log(req.user)
-  res.send('items')
-})
+
 app.listen(5000, () => {
   console.log('listening to server from port: 5000..')
 })
